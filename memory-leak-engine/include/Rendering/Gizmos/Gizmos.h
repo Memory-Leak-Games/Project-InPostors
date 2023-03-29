fd8cb34a720917accdf57a638f3136ff4daa4bff
LiptonLeon <93220207+LiptonLeon@users.noreply.github.com>
2023-03-29 16:00:22 +0200

    wip: Drawing gizmos above scene for now

--- memory-leak-engine/include/Rendering/Gizmos/Gizmos.h
+++ memory-leak-engine/include/Rendering/Gizmos/Gizmos.h
@@ -7,6 +7,22 @@ namespace mlg {
 
     class Gizmos {
     private:
+
+        struct GizmoObject {
+            uint32_t VBO;
+            uint32_t VAO;
+            uint32_t EBO = 0;
+            int32_t elements;
+            uint32_t primitive = GL_LINES;
+
+            glm::mat4 world;
+            glm::vec4 color;
+            bool alwaysFront; //TODO: Make this work if false
+            float pointSize = 1.0;
+
+            //float ttl = 0; // Time to live
+        };
+
         static uint32_t lineVBO;
         static uint32_t lineVAO;
         static uint32_t boxVBO;
@@ -15,12 +31,14 @@ namespace mlg {
         static uint32_t sphereVBO;
         static uint32_t sphereVAO;
         static uint32_t sphereEBO;
-        static uint32_t sphereIndicesCount;
+        static int32_t sphereIndicesCount;
         static uint32_t pointVBO;
         static uint32_t pointVAO;
 
         static class ShaderProgram* shader;
         static glm::vec4 defaultColor;
+
+        static std::vector<GizmoObject> gizmoInstances;
 
         static void GenerateSphere(std::vector<GLfloat>& vertices, std::vector<uint32_t>& indices, int LOD);
 
@@ -35,7 +53,7 @@ namespace mlg {
         static void DrawBox(Transform& transform, glm::vec4 color = defaultColor, bool alwaysFront = false);
         static void DrawSphere(glm::vec3 position, float radius = 1, glm::vec4 color = defaultColor, bool alwaysFront = false);
         static void DrawPoint(glm::vec3 position, glm::vec4 color = defaultColor, bool alwaysFront = false);
-        static void DrawSizedPoint(glm::vec3 position, float size = 2, glm::vec4 color = defaultColor, bool alwaysFront = false);
+        static void DrawSizedPoint(glm::vec3 position, float pointSize = 2, glm::vec4 color = defaultColor, bool alwaysFront = false);
     };
 
 }// namespace mlg
--- memory-leak-engine/src/Rendering/Gizmos/Gizmos.cpp
+++ memory-leak-engine/src/Rendering/Gizmos/Gizmos.cpp
@@ -4,62 +4,50 @@
 #include "Rendering/Assets/ShaderAsset.h"
 
 #include "Rendering/ShaderProgram.h"
+#include "Core/Time.h"
 
 using namespace mlg;
 
-GLuint Gizmos::lineVAO;
-GLuint Gizmos::lineVBO;
-GLuint Gizmos::boxVAO;
-GLuint Gizmos::boxVBO;
-GLuint Gizmos::boxEBO;
-GLuint Gizmos::sphereVAO;
-GLuint Gizmos::sphereVBO;
-GLuint Gizmos::sphereEBO;
-GLuint Gizmos::sphereIndicesCount;
-GLuint Gizmos::pointVBO;
-GLuint Gizmos::pointVAO;
+uint32_t Gizmos::lineVAO;
+uint32_t Gizmos::lineVBO;
+uint32_t Gizmos::boxVAO;
+uint32_t Gizmos::boxVBO;
+uint32_t Gizmos::boxEBO;
+uint32_t Gizmos::sphereVAO;
+uint32_t Gizmos::sphereVBO;
+uint32_t Gizmos::sphereEBO;
+int32_t Gizmos::sphereIndicesCount;
+uint32_t Gizmos::pointVBO;
+uint32_t Gizmos::pointVAO;
 
 ShaderProgram* Gizmos::shader;
 glm::vec4 Gizmos::defaultColor = {0, 1, 0, 1};
 
-GLfloat LineVertices[]{
+std::vector<Gizmos::GizmoObject> Gizmos::gizmoInstances;
+
+GLfloat lineVertices[]{
         0.0f, 0.0f, 0.0f,
         1.0f, 1.0f, 1.0f};
 
-GLfloat BoxVertices[]{
-        0.5f,
-        0.5f,
-        0.5f,
-        0.5f,
-        0.5f,
-        -0.5f,
-        -0.5f,
-        0.5f,
-        -0.5f,
-        -0.5f,
-        0.5f,
-        0.5f,
-        0.5f,
-        -0.5f,
-        0.5f,
-        0.5f,
-        -0.5f,
-        -0.5f,
-        -0.5f,
-        -0.5f,
-        -0.5f,
-        -0.5f,
-        -0.5f,
-        0.5f,
+GLfloat boxVertices[]{
+        0.5f,0.5f,0.5f,
+        0.5f,0.5f,-0.5f,
+        -0.5f,0.5f,-0.5f,
+        -0.5f,0.5f,0.5f,
+        0.5f,-0.5f,0.5f,
+        0.5f,-0.5f,-0.5f,
+        -0.5f,-0.5f,-0.5f,
+        -0.5f,-0.5f,0.5f,
 };
 
-GLuint BoxIndices[]{
+GLuint boxIndices[]{
         0, 1, 1, 2, 2, 3, 3, 0,
         4, 5, 5, 6, 6, 7, 7, 4,
-        0, 4, 1, 5, 2, 6, 3, 7};
+        0, 4, 1, 5, 2, 6, 3, 7
+};
 
 // Yes, I also find this silly
-GLfloat PointVertex[]{
+GLfloat pointVertex[]{
         0.0f, 0.0f, 0.0f};
 
 void mlg::Gizmos::Initialize() {
@@ -72,7 +60,7 @@ void mlg::Gizmos::Initialize() {
     {
         glGenBuffers(1, &lineVBO);
         glBindBuffer(GL_ARRAY_BUFFER, lineVBO);
-        glBufferData(GL_ARRAY_BUFFER, 2 * sizeof(glm::vec3), LineVertices, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, 2 * sizeof(glm::vec3), lineVertices, GL_STATIC_DRAW);
         glGenVertexArrays(1, &lineVAO);
         glBindVertexArray(lineVAO);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*) nullptr);
@@ -83,10 +71,10 @@ void mlg::Gizmos::Initialize() {
     {
         glGenBuffers(1, &boxVBO);
         glBindBuffer(GL_ARRAY_BUFFER, boxVBO);
-        glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(glm::vec3), BoxVertices, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(glm::vec3), boxVertices, GL_STATIC_DRAW);
         glGenBuffers(1, &boxEBO);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, boxEBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, 24 * sizeof(GLuint), BoxIndices, GL_STATIC_DRAW);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, 24 * sizeof(GLuint), boxIndices, GL_STATIC_DRAW);
         glGenVertexArrays(1, &boxVAO);
         glBindVertexArray(boxVAO);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLuint), (void*) nullptr);
@@ -115,7 +103,7 @@ void mlg::Gizmos::Initialize() {
     {
         glGenBuffers(1, &pointVBO);
         glBindBuffer(GL_ARRAY_BUFFER, pointVBO);
-        glBufferData(GL_ARRAY_BUFFER, 2 * sizeof(glm::vec3), PointVertex, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, 2 * sizeof(glm::vec3), pointVertex, GL_STATIC_DRAW);
         glGenVertexArrays(1, &pointVAO);
         glBindVertexArray(pointVAO);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*) nullptr);
@@ -131,94 +119,140 @@ void mlg::Gizmos::Initialize() {
 }
 
 void mlg::Gizmos::Stop() {
-    if (shader == nullptr)
-        return;
+    glDeleteBuffers(1, &lineVBO);
+    glDeleteVertexArrays(1, &lineVAO);
+    glDeleteBuffers(1, &boxVBO);
+    glDeleteVertexArrays(1, &boxVAO);
+    glDeleteBuffers(1, &boxEBO);
+    glDeleteBuffers(1, &sphereVBO);
+    glDeleteVertexArrays(1, &sphereVAO);
+    glDeleteBuffers(1, &sphereEBO);
+    glDeleteBuffers(1, &pointVBO);
+    glDeleteVertexArrays(1, &pointVAO);
+}
 
+void Gizmos::DrawGizmos() {
+    shader->Activate();
+    glEnable(GL_PROGRAM_POINT_SIZE);
+    auto gizmoEnd = gizmoInstances.end();
+    for(auto gizmoIt = gizmoInstances.begin(); gizmoIt < gizmoEnd; gizmoIt++)
+    {
+        GizmoObject gizmo = *gizmoIt;
+        shader->SetMat4F("world", gizmo.world);
+        shader->SetVec4F("color", gizmo.color);
+        shader->SetBool("alwaysFront", gizmo.alwaysFront);
+        shader->SetFloat("pointSize", gizmo.pointSize);
 
-    //TODO: delete vaos and vbos
+        glBindVertexArray(gizmo.VAO);
+        if(gizmo.EBO)
+        {
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gizmo.EBO);
+            glDrawElements(gizmo.primitive, gizmo.elements, GL_UNSIGNED_INT, nullptr);
+        }
+        else
+        {
+            glDrawArrays(gizmo.primitive, 0, gizmo.elements);
+        }
+    }
+
+    gizmoInstances.clear();
+
+    glDisable(GL_PROGRAM_POINT_SIZE);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 void mlg::Gizmos::DrawLine(glm::vec3 start, glm::vec3 end, glm::vec4 color, bool alwaysFront) {
-    glm::mat4 world = glm::mat4(1.0f);
-    world = glm::translate(world, start);
-    world = glm::scale(world, end - start);// Use position diff to scale line (which is from [0, 0, 0] to [1, 1, 1])
+    GizmoObject gizmo;
 
-    shader->Activate();
-    shader->SetMat4F("world", world);
-    shader->SetVec4F("color", color);
-    shader->SetBool("alwaysFront", alwaysFront);
+    gizmo.VBO = lineVBO;
+    gizmo.VAO = lineVAO;
+    gizmo.elements = 2;
 
-    glBindVertexArray(lineVAO);
-    glDrawArrays(GL_LINES, 0, 2);
-    glBindVertexArray(0);
-}
+    gizmo.world = glm::mat4(1.0f);
+    gizmo.world = glm::translate(gizmo.world, start);
+    gizmo.world = glm::scale(gizmo.world, end - start); // Use position diff to scale line (which is from [0, 0, 0] to [1, 1, 1])
 
-void Gizmos::DrawBox(Transform& transform, glm::vec4 color, bool alwaysFront) {
-    const glm::mat4 world = transform.GetLocalMatrix();
+    gizmo.color = color;
+    gizmo.alwaysFront = alwaysFront;
 
-    shader->Activate();
-    shader->SetMat4F("world", world);
-    shader->SetVec4F("color", color);
-    shader->SetBool("alwaysFront", alwaysFront);
-
-    glBindVertexArray(boxVAO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, boxEBO);
-    glDrawElements(GL_LINES, 24, GL_UNSIGNED_INT, nullptr);
-    glBindVertexArray(0);
+    gizmoInstances.push_back(gizmo);
 }
 
 void Gizmos::DrawBox(glm::vec3 position, glm::vec3 size, glm::quat rotation, glm::vec4 color, bool alwaysFront) {
-    glm::mat4 world = glm::mat4(1.0f);
-    world = glm::translate(world, position);
-    world = world * glm::toMat4(rotation);
-    world = glm::scale(world, size);// Default size is 1x1x1
+    GizmoObject gizmo;
 
-    shader->Activate();
-    shader->SetMat4F("world", world);
-    shader->SetVec4F("color", color);
-    shader->SetBool("alwaysFront", alwaysFront);
+    gizmo.VBO = boxVBO;
+    gizmo.VAO = boxVAO;
+    gizmo.EBO = boxEBO;
+    gizmo.elements = 24;
 
-    glBindVertexArray(boxVAO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, boxEBO);
-    glDrawElements(GL_LINES, 24, GL_UNSIGNED_INT, nullptr);
-    glBindVertexArray(0);
+    gizmo.world = glm::mat4(1.0f);
+    gizmo.world = glm::translate(gizmo.world, position);
+    gizmo.world = gizmo.world * glm::toMat4(rotation);
+    gizmo.world = glm::scale(gizmo.world, size); // Default size is 1x1x1
+
+    gizmo.color = color;
+    gizmo.alwaysFront = alwaysFront;
+
+    gizmoInstances.push_back(gizmo);
+}
+
+void Gizmos::DrawBox(Transform& transform, glm::vec4 color, bool alwaysFront) {
+    GizmoObject gizmo;
+
+    gizmo.VBO = boxVBO;
+    gizmo.VAO = boxVAO;
+    gizmo.EBO = boxEBO;
+    gizmo.elements = 24;
+
+    gizmo.world = transform.GetLocalMatrix();
+
+    gizmo.color = color;
+    gizmo.alwaysFront = alwaysFront;
+
+    gizmoInstances.push_back(gizmo);
 }
 
 void Gizmos::DrawSphere(glm::vec3 position, float radius, glm::vec4 color, bool alwaysFront) {
-    glm::mat4 world = glm::mat4(1.0f);
-    world = glm::translate(world, position);
-    world = glm::scale(world, glm::vec3(radius * 2));// Default radius is 1
+    GizmoObject gizmo;
 
-    shader->Activate();
-    shader->SetMat4F("world", world);
-    shader->SetVec4F("color", color);
-    shader->SetBool("alwaysFront", alwaysFront);
+    gizmo.VBO = sphereVBO;
+    gizmo.VAO = sphereVAO;
+    gizmo.EBO = sphereEBO;
+    gizmo.elements = sphereIndicesCount;
 
-    glBindVertexArray(sphereVAO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sphereEBO);
-    glDrawElements(GL_LINES, sphereIndicesCount, GL_UNSIGNED_INT, nullptr);
-    glBindVertexArray(0);
+    gizmo.world = glm::mat4(1.0f);
+    gizmo.world = glm::translate(gizmo.world, position);
+    gizmo.world = glm::scale(gizmo.world, glm::vec3(radius * 2)); // Default radius is 1
+
+    gizmo.color = color;
+    gizmo.alwaysFront = alwaysFront;
+
+    gizmoInstances.push_back(gizmo);
+
 }
 
 void Gizmos::DrawPoint(glm::vec3 position, glm::vec4 color, bool alwaysFront) {
     DrawSizedPoint(position, 2, color, alwaysFront);
 }
 
-void Gizmos::DrawSizedPoint(glm::vec3 position, float size, glm::vec4 color, bool alwaysFront) {
-    glm::mat4 world = glm::mat4(1.0f);
-    world = glm::translate(world, position);
+void Gizmos::DrawSizedPoint(glm::vec3 position, float pointSize, glm::vec4 color, bool alwaysFront) {
+    GizmoObject gizmo;
 
-    shader->Activate();
-    shader->SetMat4F("world", world);
-    shader->SetVec4F("color", color);
-    shader->SetBool("alwaysFront", alwaysFront);
-    shader->SetFloat("pointSize", size);
+    gizmo.VBO = pointVBO;
+    gizmo.VAO = pointVAO;
+    gizmo.elements = 1;
+    gizmo.primitive = GL_POINTS;
 
-    glEnable(GL_PROGRAM_POINT_SIZE);
-    glBindVertexArray(pointVAO);
-    glDrawArrays(GL_POINTS, 0, 1);
-    glBindVertexArray(0);
-    glDisable(GL_PROGRAM_POINT_SIZE);
+    gizmo.world = glm::mat4(1.0f);
+    gizmo.world = glm::translate(gizmo.world, position);
+
+    gizmo.color = color;
+    gizmo.alwaysFront = alwaysFront;
+    gizmo.pointSize = pointSize;
+
+    gizmoInstances.push_back(gizmo);
 }
 
 // FIXME: It's not a sphere :/
@@ -239,23 +273,19 @@ void Gizmos::GenerateSphere(std::vector<GLfloat>& vertices, std::vector<GLuint>&
             double x = glm::cos(lng);
             double y = glm::sin(lng);
 
-            vertices.push_back(x * zr0 * 0.5);
-            vertices.push_back(y * zr0 * 0.5);
-            vertices.push_back(z0 * 0.5);
+            vertices.push_back(x * zr0 * 0.5f);
+            vertices.push_back(y * zr0 * 0.5f);
+            vertices.push_back(z0 * 0.5f);
             indices.push_back(indicator);
             indicator++;
 
-            vertices.push_back(x * zr1 * 0.5);
-            vertices.push_back(y * zr1 * 0.5);
-            vertices.push_back(z1 * 0.5);
+            vertices.push_back(x * zr1 * 0.5f);
+            vertices.push_back(y * zr1 * 0.5f);
+            vertices.push_back(z1 * 0.5f);
             indices.push_back(indicator);
             indicator++;
         }
     }
 
     sphereIndicesCount = indices.size();
-}
-
-void Gizmos::DrawGizmos() {
-
 }
