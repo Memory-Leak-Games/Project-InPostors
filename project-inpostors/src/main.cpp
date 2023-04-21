3dc8d691b5363ff424feb4eb0c29fbbfcc7e7b17
Szymon Świędrych <43550433+S1NU5-P1@users.noreply.github.com>
2023-04-21 20:33:48 +0200

    Kill old collision detection at 350 colliders

--- project-inpostors/src/main.cpp
+++ project-inpostors/src/main.cpp
@@ -7,26 +7,26 @@
 #include <Rendering/Assets/MaterialAsset.h>
 #include <Rendering/Assets/ModelAsset.h>
 
-#include <Core/HID/Input.h>
 #include <Core/AssetManager/AssetManager.h>
+#include <Core/HID/Input.h>
 
 #include "Core/Core.h"
 #include "Core/Time.h"
 
-#include "SceneGraph/SceneGraph.h"
-#include "Player.h"
 #include "Core/Settings/SettingsManager.h"
 #include "Gameplay/Components/CameraComponent.h"
 #include "Gameplay/Levels/LevelGenerator.h"
+#include "Player.h"
+#include "SceneGraph/SceneGraph.h"
 
+#include <Core/Math.h>
 #include <Gameplay/ComponentManager.h>
+#include <Gameplay/Components/RigidbodyComponent.h>
 #include <Gameplay/Components/StaticMeshComponent.h>
 #include <Gameplay/EntityManager.h>
+#include <Physics/Physics.h>
 #include <Rendering/CommonUniformBuffer.h>
 #include <Rendering/Gizmos/Gizmos.h>
-#include <Physics/Physics.h>
-#include <Core/Math.h>
-#include <Gameplay/Components/RigidbodyComponent.h>
 
 #include <UI/Assets/FontAsset.h>
 #include <UI/Components/Image.h>
@@ -36,7 +36,7 @@
 
 class ComponentTest : public mlg::Component {
 public:
-    ComponentTest(const std::weak_ptr<mlg::Entity> &owner, const std::string &name) : Component(owner, name) {}
+    ComponentTest(const std::weak_ptr<mlg::Entity>& owner, const std::string& name) : Component(owner, name) {}
 
     void Update() override {
         glm::vec3 position = GetOwner().lock()->GetTransform().GetPosition();
@@ -51,7 +51,6 @@ public:
     }
 
     ~ComponentTest() override {
-
     }
 };
 
@@ -59,7 +58,7 @@ class ProjectInpostors {
 public:
     ProjectInpostors() = default;
 
-    int Main(int argc, char *argv[]) {
+    int Main(int argc, char* argv[]) {
         mlg::SettingsManager::Initialize();
 
         mlg::Time::Initialize();
@@ -80,7 +79,7 @@ public:
         mlg::Core::Initialize();
         mlg::Input::Initialize();
 
-        mlg::Core *engine = mlg::Core::GetInstance();
+        mlg::Core* engine = mlg::Core::GetInstance();
         PrepareScene();
         engine->MainLoop();
 
@@ -108,7 +107,7 @@ public:
     void PrepareScene() {
         auto cameraEntity = mlg::EntityManager::SpawnEntity<mlg::Entity>("Camera", false, mlg::SceneGraph::GetRoot());
         auto cameraComponent = cameraEntity.lock()->AddComponent<mlg::CameraComponent>("CameraComponent");
-//        cameraComponent.lock()->SetPerspective(glm::radians(90.f), 0.1, 100.f);
+        //        cameraComponent.lock()->SetPerspective(glm::radians(90.f), 0.1, 100.f);
         cameraComponent.lock()->SetOrtho(40.f, 0.1, 100.f);
 
         cameraComponent.lock()->GetTransform().SetPosition({-10.f, 15.f, -10.f});
@@ -140,8 +139,7 @@ public:
         image.lock()->SetSize(glm::vec2{256.f});
         image.lock()->SetPosition({50.f, 50.f});
 
-        auto progressBarMaterial = mlg::AssetManager::GetAsset<mlg::MaterialAsset>(
-                "res/materials/UI/progressBar_material.json");
+        auto progressBarMaterial = mlg::AssetManager::GetAsset<mlg::MaterialAsset>("res/materials/UI/progressBar_material.json");
         auto progressBar = ui.lock()->AddComponent<mlg::ProgressBar>("ProgressBar", progressBarMaterial);
         progressBar.lock()->SetSize(glm::vec2{256.f, 32.f});
         progressBar.lock()->SetPosition({50.f, 400.f});
@@ -150,71 +148,38 @@ public:
         mlg::Renderer2D::GetInstance()->AddRenderable(image);
         mlg::Renderer2D::GetInstance()->AddRenderable(progressBar);
 
-        auto wall = mlg::EntityManager::SpawnEntity<mlg::Entity>("Wall", true, mlg::SceneGraph::GetRoot());
-        wall.lock()->AddComponent<mlg::StaticMeshComponent>("StaticMesh", cubeModel, redMaterial);
-        wall.lock()->GetTransform().SetPosition({-2.f, 0.f, -5.f});
-        auto rigidbody = wall.lock()->AddComponent<mlg::RigidbodyComponent>("Rigidbody");
-        rigidbody.lock()->AddCollider<mlg::ColliderShape::Rectangle>(glm::vec2(0.f), glm::vec2(1.f));
-
-        auto box = mlg::EntityManager::SpawnEntity<mlg::Entity>("Box", false, mlg::SceneGraph::GetRoot());
-        box.lock()->AddComponent<mlg::StaticMeshComponent>("StaticMesh", cubeModel, blueMaterial);
-        box.lock()->GetTransform().SetPosition({2.f, 0.f, -5.f});
-        auto boxRigidbody = box.lock()->AddComponent<mlg::RigidbodyComponent>("Rigidbody");
-        boxRigidbody.lock()->AddCollider<mlg::ColliderShape::Rectangle>(glm::vec2(0.f), glm::vec2(1.f));
-        boxRigidbody.lock()->SetLinearDrag(2.f);
-        boxRigidbody.lock()->SetAngularDrag(2.f);
-
-        auto sphere = mlg::EntityManager::SpawnEntity<mlg::Entity>("Sphere", false, mlg::SceneGraph::GetRoot());
-        auto sphereMesh = sphere.lock()->AddComponent<mlg::StaticMeshComponent>("StaticMesh", sphereModel,
-                                                                                blueMaterial);
-        sphereMesh.lock()->GetTransform().SetScale(glm::vec3{2.f});
-        sphere.lock()->GetTransform().SetPosition({2.f, 0.f, 5.f});
-        auto sphereRigidbody = sphere.lock()->AddComponent<mlg::RigidbodyComponent>("Rigidbody");
-        sphereRigidbody.lock()->AddCollider<mlg::ColliderShape::Circle>(glm::vec2(0.f), 1.f);
-        sphereRigidbody.lock()->SetLinearDrag(2.f);
-        sphereRigidbody.lock()->SetAngularDrag(2.f);
-
         auto player = mlg::EntityManager::SpawnEntity<Player>("Player", false, mlg::SceneGraph::GetRoot());
 
-//        SpawnHouses();
-        mlg::LevelGenerator::LoadJson("res/levels/detroit.json");
-        mlg::LevelGenerator::GenerateLevel(7.0f);
+        SpawnSpheres();
+
+        //        mlg::LevelGenerator::LoadJson("res/levels/detroit.json");
+        //        mlg::LevelGenerator::GenerateLevel(7.0f);
     }
 
-    void SpawnHouses() {
-        using Random = effolkronium::random_static;
+    void SpawnSpheres() {
+        const int size = 350;
+        const int sizeX = std::floor(std::sqrt(size));
+        const int sizeY = size / sizeX;
 
-        std::vector<std::shared_ptr<mlg::ModelAsset>> models;
-        models.push_back(mlg::AssetManager::GetAsset<mlg::ModelAsset>("res/models/Buildings/House_1.obj"));
-        models.push_back(mlg::AssetManager::GetAsset<mlg::ModelAsset>("res/models/Buildings/House_2.obj"));
-        models.push_back(mlg::AssetManager::GetAsset<mlg::ModelAsset>("res/models/Buildings/House_3.obj"));
+        const float distance = 3.f;
 
-        float size = 10.f;
-        int city_size = 3;
-        float city_size_in_units = (float) city_size * size;
+        auto sphereModel = mlg::AssetManager::GetAsset<mlg::ModelAsset>("res/models/Primitives/Sphere.obj");
+        auto blueMaterial = mlg::AssetManager::GetAsset<mlg::MaterialAsset>("res/models/Primitives/blue_material.json");
 
-        for (int i = 0; i < city_size; ++i) {
-            for (int j = 0; j < city_size; ++j) {
-                if (i % 3)
-                    Random::shuffle(models);
+        for (int i = 0; i < sizeX; ++i) {
+            for (int j = 0; j < sizeY; ++j) {
+                glm::vec2 offset = {sizeX * distance / 2.f, sizeY * distance / 2.f};
 
-                auto whiteMaterial = mlg::AssetManager::GetAsset<mlg::MaterialAsset>(
-                        "res/models/Primitives/white_material.json");
-                auto model = models[i % 3];
+                auto sphere = mlg::EntityManager::SpawnEntity<mlg::Entity>("Sphere", false, mlg::SceneGraph::GetRoot());
+                sphere.lock()->GetTransform().SetPosition({(float) i * distance - offset.x, 0.f, (float) j * distance - offset.y});
 
-                glm::vec3 buildingPosition{0.f};
-                buildingPosition.x = (float) i * size - city_size_in_units / 2.f;
-                buildingPosition.y = -0.5;
-                buildingPosition.z = (float) j * size - city_size_in_units / 2.f;
+                auto sphereMesh = sphere.lock()->AddComponent<mlg::StaticMeshComponent>("StaticMesh", sphereModel, blueMaterial);
+                sphereMesh.lock()->GetTransform().SetScale(glm::vec3{2.f});
 
-                auto building = mlg::EntityManager::SpawnEntity<mlg::Entity>("Wall", true, mlg::SceneGraph::GetRoot());
-                building.lock()->GetTransform().SetPosition(buildingPosition);
-                building.lock()->GetTransform().SetScale(glm::vec3(2.f));
-
-                building.lock()->AddComponent<mlg::StaticMeshComponent>("StaticMesh", model, whiteMaterial);
-                auto rigidbody = building.lock()->AddComponent<mlg::RigidbodyComponent>("Rigidbody");
-                rigidbody.lock()->AddCollider<mlg::ColliderShape::Rectangle>(glm::vec2(0.f), glm::vec2(4.f));
-                rigidbody.lock()->SetBounciness(0.2f);
+                auto sphereRigidbody = sphere.lock()->AddComponent<mlg::RigidbodyComponent>("Rigidbody");
+                sphereRigidbody.lock()->AddCollider<mlg::ColliderShape::Circle>(glm::vec2(0.f), 1.f);
+                sphereRigidbody.lock()->SetLinearDrag(2.f);
+                sphereRigidbody.lock()->SetAngularDrag(2.f);
             }
         }
     }
@@ -223,7 +188,7 @@ public:
     }
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     LoggingMacros::InitializeSPDLog();
 
     ProjectInpostors game;
