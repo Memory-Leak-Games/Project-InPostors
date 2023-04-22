a0a0b5292992931986cc9fb5c33e8fdaf4a98cf3
Szymon Świędrych <43550433+S1NU5-P1@users.noreply.github.com>
2023-04-22 12:00:50 +0200

    Fix cmake

--- cmake/global_settings.cmake
+++ cmake/global_settings.cmake
@@ -1,3 +1,5 @@
+include(CMakePrintHelpers)
+
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 set_property(GLOBAL PROPERTY PREDEFINED_TARGETS_FOLDER "CMake")
 
@@ -12,13 +14,13 @@ set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
 set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 
-set(BUILD_SHARED_LIBS                       OFF CACHE INTERNAL "Build package with shared libraries.")
-set(ASSIMP_BUILD_ASSIMP_TOOLS               OFF CACHE INTERNAL "If the supplementary tools for Assimp are built in addition to the library.")
-set(ASSIMP_NO_EXPORT                        ON CACHE INTERNAL  "Disable Assimp export")
-set(ASSIMP_BUILD_ALL_EXPORTERS_BY_DEFAULT   OFF CACHE INTERNAL "Disable Assimp export")
-set(ASSIMP_BUILD_ALL_IMPORTERS_BY_DEFAULT   OFF CACHE INTERNAL "Disable Assimp export")
-set(ASSIMP_BUILD_TESTS                      OFF CACHE INTERNAL "If the test suite for Assimp is built in addition to the library.")
-set(ASSIMP_BUILD_TESTS                      OFF CACHE INTERNAL "If the test suite for Assimp is built in addition to the library.")
+set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "Build package with shared libraries.")
+set(ASSIMP_BUILD_ASSIMP_TOOLS OFF CACHE INTERNAL "If the supplementary tools for Assimp are built in addition to the library.")
+set(ASSIMP_NO_EXPORT ON CACHE INTERNAL "Disable Assimp export")
+set(ASSIMP_BUILD_ALL_EXPORTERS_BY_DEFAULT OFF CACHE INTERNAL "Disable Assimp export")
+set(ASSIMP_BUILD_ALL_IMPORTERS_BY_DEFAULT OFF CACHE INTERNAL "Disable Assimp export")
+set(ASSIMP_BUILD_TESTS OFF CACHE INTERNAL "If the test suite for Assimp is built in addition to the library.")
+set(ASSIMP_BUILD_TESTS OFF CACHE INTERNAL "If the test suite for Assimp is built in addition to the library.")
 
 # You can change supported importers here
 set(ASSIMP_BUILD_OBJ_IMPORTER ON)
@@ -27,12 +29,16 @@ set(ASSIMP_BUILD_OBJ_IMPORTER ON)
 #set(ASSIMP_BUILD_FBX_IMPORTER ON)
 
 if (UNIX)
-    execute_process(COMMAND "loginctl show-session $(loginctl | grep $(whoami) | awk '{print $1}') -p Type" OUTPUT_VARIABLE result_display_server)
-    if ("${resulting_display_server}" STREQUAL "Type=x11")
+    execute_process(COMMAND bash -c "loginctl show-session $(loginctl | grep $(whoami) | awk '{print $1}') -p Type"
+                    OUTPUT_VARIABLE result_display_server
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+    cmake_print_variables(result_display_server)
+
+    if ("${result_display_server}" STREQUAL "Type=wayland")
+        message(Using Wayland)
+        set(GLFW_USE_WAYLAND ON)
+    else ()
         message(Using X11)
-        set(GLFW_USE_WAYLAND OFF CACHE BOOL "" FORCE)
-    else()
-        message(Uising Wayland)
-        set(GLFW_USE_WAYLAND ON CACHE BOOL "" FORCE)
-    endif()
+        set(GLFW_USE_WAYLAND OFF)
+    endif ()
 endif ()
