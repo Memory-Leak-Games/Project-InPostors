c33b4084b71be4260eaea9cc973ebe483efe2d3d
236662 <236662@edu.p.lodz.pl>
2023-04-23 18:00:16 +0200

    fix: Small change so it hopefully builds on github

--- memory-leak-engine/src/Gameplay/Components/AudioComponent.cpp
+++ memory-leak-engine/src/Gameplay/Components/AudioComponent.cpp
@@ -1,6 +1,6 @@
 #include "include/Gameplay/Components/AudioComponent.h"
 
-#include "audio/Assets/AudioAsset.h"
+#include "Audio/Assets/AudioAsset.h" 
 
 namespace mlg {
     AudioComponent::AudioComponent(const std::weak_ptr<Entity>& owner, const std::string& name, const std::shared_ptr<AudioAsset>& audio) : Component(owner, name), audio(audio) {
