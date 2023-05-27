3a3ed85a5e69e715c44cf40ed6a9bffe0832ab9c
Szymon Świędrych <43550433+S1NU5-P1@users.noreply.github.com>
2023-05-27 19:40:45 +0200

    Add sublevels

--- project-inpostors/src/main.cpp
+++ project-inpostors/src/main.cpp
@@ -137,17 +137,13 @@ public:
         std::vector<std::string> levelLayout;
 
         const std::string testLevelPath = "res/levels/maps/detroit.json";
-        const std::string testLevelPropsPath = "res/levels/maps/detroit_props.json";
 
         levelLayout = mlg::LevelGenerator::LoadMap(testLevelPath);
-        mlg::LevelGenerator::LoadMap(testLevelPath);
         mlg::LevelGenerator::SpawnGround(testLevelPath);
         mlg::LevelGenerator::SetCityBounds(testLevelPath);
         mlg::LevelGenerator::LoadCameraSettings(testLevelPath, *cameraComponent.lock());
         mlg::LevelGenerator::SpawnPlayers(testLevelPath);
 
-        // load props
-        mlg::LevelGenerator::LoadMap(testLevelPropsPath);
 
         // create factories
         //auto testFactory = mlg::EntityManager::SpawnEntity<Factory>("Smelter", false, mlg::SceneGraph::GetRoot(),
