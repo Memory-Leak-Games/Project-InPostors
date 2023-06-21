36a1f70974de20ae393440640cf6c3e38f58ea0d
Szymon Świędrych <43550433+S1NU5-P1@users.noreply.github.com>
2023-06-21 20:22:52 +0200

    Add storage ui animation

--- project-inpostors/include/UI/StorageUI.h
+++ project-inpostors/include/UI/StorageUI.h
@@ -1,13 +1,19 @@
 #pragma once
 
-#include "Gameplay/Component.h"
+#include "Gameplay/SceneComponent.h"
 
 namespace mlg {
-    class Entity;
+    class Image;
 }
 
-class StorageUI : public mlg::Component {
-public:
-    StorageUI(const std::weak_ptr<class mlg::Entity>& owner, const std::string& name);
+class StorageUI : public mlg::SceneComponent {
+    std::shared_ptr<mlg::Image> pin;
 
+    bool animate = false;
+    float time = 0.f;
+public:
+    StorageUI(const std::weak_ptr<mlg::Entity>& owner, const std::string& name);
+
+    void Start() override;
+    void Update() override;
 };
