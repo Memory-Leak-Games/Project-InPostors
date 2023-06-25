4f37803b7e565b0d744674b1172bbb97bddf3540
Szymon Świędrych <43550433+S1NU5-P1@users.noreply.github.com>
2023-06-25 22:19:45 +0200

    Add image to tutorial panel

---
+++ memory-leak-engine/src/UI/Components/DynamicImage.cpp
@@ -0,0 +1,20 @@
+#include "UI/Components/DynamicImage.h"
+#include "Core/AssetManager/AssetManager.h"
+#include "Rendering/Assets/MaterialAsset.h"
+#include "Rendering/Assets/TextureAsset.h"
+
+mlg::DynamicImage::DynamicImage(std::weak_ptr<Entity> owner, std::string name)
+    : Image(owner, std::move(name), nullptr) {
+    material = mlg::AssetManager::GetAsset<mlg::MaterialAsset>(
+            "res/materials/ui/image_material.json");
+    material = material->CreateDynamicInstance();
+
+    std::shared_ptr<TextureAsset> texture =
+            mlg::AssetManager::GetAsset<mlg::TextureAsset>(
+                    "res/textures/error_texture.png");
+    material->SetTexture("image", texture);
+}
+
+void mlg::DynamicImage::SetTexture(const std::shared_ptr<TextureAsset>& texture) {
+    material->SetTexture("image", texture);
+}
